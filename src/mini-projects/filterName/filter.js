8be663e448209d60e99e30cabde3dc5ec7ba1a5e
Mahdi Harooni <mahdiharooniir@gmail.com>
2025-09-12 13:59:02 +0330

    Do filterName mint-project

---
+++ src/mini-projects/filterName/filter.js
@@ -0,0 +1,79 @@
+const users = [
+    {
+        id: 1,
+        firstName: "مهدی",
+        lastName: "هارونی",
+        age: 28,
+        gender: "مرد"
+    },
+    {
+        id: 2,
+        firstName: "اصغر",
+        lastName: "رضوی",
+        age: 40,
+        gender: "مرد"
+    },
+    {
+        id: 3,
+        firstName: "مهسا",
+        lastName: "بهرامی",
+        age: 15,
+        gender: "زن"
+    },
+    {
+        id: 4,
+        firstName: "الهه",
+        lastName: "خسروی",
+        age: 34,
+        gender: "زن"
+    },
+    {
+        id: 5,
+        firstName: "مهدی",
+        lastName: "محمدی",
+        age: 27,
+        gender: "مرد"
+    },
+    {
+        id: 5,
+        firstName: "امیر",
+        lastName: "ارجمند",
+        age: 47,
+        gender: "مرد"
+    },
+]
+
+const userContainer = document.querySelector('#userContainer');
+const filterInput = document.querySelector('#filterInput')
+const filterBtn = document.querySelector('#filterBtn');
+
+window.onload = function () {
+    userContainer.innerHTML = generateUsersElement(users) ;
+}
+
+
+function generateUsersElement(users) {
+    let str = '';
+    users.forEach(user => str += `
+        <div class="shadow-sm shadow-sky-300 rounded-lg p-4 text-md bg-sky-50">
+            <p>نام: ${user.firstName}</p>
+            <p>نام خانوادگی: ${user.lastName}</p>
+            <p>سن: ${user.age}</p>
+            <p>جنسیت : ${user.gender}</p>
+        </div>
+        `);
+        return str;
+}
+
+filterBtn.onclick = function() {
+    const filteredUsers = users.filter(function(user) {
+        return user.firstName.includes(filterInput.value)
+    })
+    if (filteredUsers.length > 0) {
+        userContainer.innerHTML = generateUsersElement(filteredUsers)
+    } else {
+        userContainer.innerHTML = "<h4> کاربر یافت نشد </h4"
+    }
+}
+
+
