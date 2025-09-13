5806593c0752f7d5d47b337887e910a59878282d
Mahdi Harooni <mahdiharooniir@gmail.com>
2025-09-13 10:47:52 +0330

    Do random color using the second method

--- js-course/chapter-11/randomColorInJS/app.js
+++ js-course/chapter-11/randomColorInJS/app.js
@@ -1,3 +1,4 @@
+// way 1
 function getRandomColor() {
     let letters = '0123456789ABCDEF'
     let color = '#'
@@ -11,3 +12,17 @@ const toggleBtn = document.querySelector('#toggleBtn');
 toggleBtn.onclick = function() {
     document.body.style.backgroundColor = getRandomColor();
 }
+
+
+// // way 2
+// function randNum(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+// function randColor() {
+//     return `rgb(${randNum(0, 255)}, ${randNum(0, 255)}, ${randNum(0, 255)})`
+// }
+
+// function changeBodyColor() {
+//     document.body.style.backgroundColor = randColor()
+// }
