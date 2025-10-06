e98c22156b2614e00d8a91dfb7d2d4163f367091
Mahdi Harooni <mahdiharooniir@gmail.com>
2025-10-06 17:15:56 +0330

    Refactor count-click-screen

--- src/practice/chapter-11/count-click-screen/app.js
+++ src/practice/chapter-11/count-click-screen/app.js
@@ -1,7 +1,7 @@
 const pElement = document.querySelector('.number');
 
 let number = 0
-document.body.onclick = function() {
+document.body.addEventListener( 'click', () => {
     number++
     pElement.innerHTML = number
-}
+})
