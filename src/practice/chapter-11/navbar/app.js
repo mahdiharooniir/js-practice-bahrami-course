5b6f137f58573deb6fd42cf2dd962a393e55b610
Mahdi Harooni <mahdiharooniir@gmail.com>
2025-10-07 00:14:18 +0330

    Refactor navbar practice

--- src/practice/chapter-11/navbar/app.js
+++ src/practice/chapter-11/navbar/app.js
@@ -1,21 +1,20 @@
 const mobileMenu = document.querySelector('.mobileMenu')
 const toggleBtn = document.querySelector('button')
 
-toggleBtn.onclick = function() {
-    if (mobileMenu.classList.contains('hidden')) {
+toggleBtn.addEventListener('click' , () => {
+    mobileMenu.classList.toggle('hidden')
+    const isHidden = mobileMenu.classList.contains('hidden')
+    if (isHidden) {
+        toggleBtn.innerHTML = `
+        <span>منو</span>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+        </svg>`
+    } else {
         toggleBtn.innerHTML = `
         <span>منو</span>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
             <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
-        </svg>
-        `
-    } else {
-        toggleBtn.innerHTML = `
-            <span>منو</span>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
-                <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-            </svg>`
+        </svg>`
     }
-    mobileMenu.classList.toggle('hidden')
-}
-
+})
